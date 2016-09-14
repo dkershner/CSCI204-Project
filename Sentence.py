38ffdbdfab037b471d9ef471c8ee4b767ce573d5
dkershner <dtk008@bucknell.edu>
2016-09-14 17:34:13 -0400

    Update Sentence.py

--- Sentence.py
+++ Sentence.py
@@ -1,2 +1,12 @@
 class Sentence:
-    #stuff
+    
+    def __init__(self, sentence):
+        self.wordCount
+        self.charCount
+        self.punctuation
+        
+    def parseWords(self):
+        return self.split()
+        
+    def parseChar(self):
+        return list(self)
