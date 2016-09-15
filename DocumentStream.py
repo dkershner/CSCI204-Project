368f761a58fa57e8046953e92259afa49f54983c
dkershner <dtk008@bucknell.edu>
2016-09-14 23:28:24 -0400

    Update DocumentStream.py

--- DocumentStream.py
+++ DocumentStream.py
@@ -1,7 +1,12 @@
 class DocumentStream:
     
+    def __init__(self, filename = none):
+        self.filename = filename
+    
     def readWhole():
-        
+        file = open(self.fileName, 'r', encoding='utf-8' )
+        lines = file.readlines()
+        print(lines)
 
 
 
